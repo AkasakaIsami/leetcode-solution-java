7a0babe3957dac37b0f6ddaa92c4855b5368fe0b
AkasakaIsami <akasaka0623@163.com>
2022-06-27 14:56:42 +0800

    INIT OFFER56

---
+++ offer56-1-shu-zu-zhong-shu-zi-chu-xian-de-ci-shu/src/Solution.java
@@ -0,0 +1,11 @@
+/**
+ * @author Akasaka Isami
+ * @description
+ * @date 2022-06-27 13:58:25
+ * @URL https://leetcode.cn/problems/shu-zu-zhong-shu-zi-chu-xian-de-ci-shu-lcof/
+ */
+public class Solution {
+    public int[] singleNumbers(int[] nums) {
+
+    }
+}
